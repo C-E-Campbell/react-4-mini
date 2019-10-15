7b203b4749762289698a6e82c3510dab71aa28f1
charles <codelyfewithcharlie@gmail.com>
2019-10-15 13:35:00 -0700

    started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
+import { HashRouter as Router } from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Nav />
-        <h1 style={{ padding: "200px 35%" }}>
-          This is where your pages will appear
-        </h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<Nav />
+				<h1 style={{ padding: "200px 35%" }}>
+					This is where your pages will appear
+				</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
